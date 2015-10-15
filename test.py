533cb52514ae0cd824052cf7f96ba0d7d74227f1
Administrator <admin@example.com>
2015-10-15 14:31:49 -0500

    add test for myout

--- test.py
+++ test.py
@@ -6,6 +6,9 @@ class TestMethods(unittest.TestCase):
   def test_myput(self):
       self.assertEqual(app.myput(5), 5)
       
+  def test_myout(self):
+      self.assertEqual(app.myout(5), 5)
+      
 
 if __name__ == '__main__':
     unittest.main()
